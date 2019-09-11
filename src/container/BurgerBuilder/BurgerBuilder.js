93afd5b3f135dc347307dd8d476605d4f3db9a45
tiansiyu0210 <tiansiyu297@gmail.com>
2019-09-11 11:17:45 -0400

    209. Sending a POST Request

--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../component/Burger/Burger';
 import BuilderControls from '../../component/Burger/BuildControls/BuildControls'
 import Modal from '../../component/UI/Modal/Modal';
 import OrderSummary from '../../component/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICE ={
     bacon: 1.7,
@@ -87,7 +88,28 @@ class BurgerBuilder extends Component {
     }
 
     payOrderHandler = () => {
-        alert('you pay successfully');
+        //alert('you pay successfully');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.price,
+            customer: {
+                name: 'tian',
+                address: {
+                    street: 'moringside',
+                    zipCode: '22332'
+                },
+                email: 'test@gmail.com',
+            },
+            deliveryMethod: 'fast'
+        };
+
+        axios.post('/orders.json', order)
+            .then( response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     render() {
