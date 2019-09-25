c21209b717ed8ba42a508b935494421d7d361870
tiansiyu0210 <tiansiyu297@gmail.com>
2019-09-25 09:34:28 -0400

    264. Creating a Custom Dynamic Input Component

--- src/container/Checkout/ContactData/ContactData.js
+++ src/container/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../../../component/UI/Button/Button';
 import classes from './ContactData.module.css'
 import axios from '../../../axios-orders';
 import Spinner from '../../../component/UI/Spinner/Spinner';
+import Input from '../../../component/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
@@ -48,10 +49,10 @@ class ContactData extends Component {
     render() {
         let form = (
             <form>
-                <input type="text" name="name" placeholder="your name"/>
-                <input type="email" name="email" placeholder="your email"/>
-                <input type="street" name="street" placeholder="your street"/>
-                <input type="zip" name="zip" placeholder="your zip"/>
+                <Input inputtype="input" type="text" name="name" placeholder="your name"/>
+                <Input inputtype="input" type="email" name="email" placeholder="your email"/>
+                <Input inputtype="input" type="street" name="street" placeholder="your street"/>
+                <Input inputtype="input" type="zip" name="zip" placeholder="your zip"/>
                 <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
             </form>
         );
